9d017ebac00eba48b15e16d4aec74ca724149b70
Marco De Paoli <depaolim@gmail.com>
2023-03-26 14:50:24 +0200

    draft: first steps

---
+++ Makefile
@@ -0,0 +1,2 @@
+draft_img:
+	(cd draft; ../utils/venv/bin/python ../utils/scale_down.py ../utils/original_images)
--- utils/scale_down.py
+++ utils/scale_down.py
@@ -3,33 +3,37 @@ import sys
 
 from PIL import Image
 
-SRC_DIR = "original_images"
-DESTS = (
-    ("img_big", (800, 800)),
-    ("img", (300, 300)),
-)
+DESTS = {
+    2: {"img": (400, 400)},
+    3: {"img": (500, 500)},
+}
+
+DEST_DEFAULTS = {
+    "img_big": (800, 800),
+    "img": (300, 300),
+}
 
 
-def resize(name):
-    print(name)
-    src = os.path.join(SRC_DIR, name)
-    im = Image.open(src)
-    for dest_dir, max_size in DESTS:
-        dst = os.path.join(dest_dir, name)
+def resize(src_dir, name, extension):
+    src_path = os.path.join(src_dir, name + extension)
+    im = Image.open(src_path)
+    dests = DESTS.get(int(name), {})
+    for dest_dir, max_size_default in DEST_DEFAULTS.items():
+        max_size = dests.get(dest_dir, max_size_default)
+        dst_path = os.path.join(dest_dir, name + extension)
+        print(dst_path, max_size)
         im.thumbnail(max_size, Image.LANCZOS)
-        im.save(dst)
+        # im.resize(max_size, Image.LANCZOS)
+        im.save(dst_path)
 
 
-def main(*names):
-    if names:
-        for name in names:
-            resize(name)
-    else:
-        print("PROCESS ALL IMAGES")
-        fs = os.listdir(SRC_DIR)
-        for name in fs:
-            if "jpg" in name:
-                resize(name)
+def main(src_dir):
+    print("PROCESS ALL IMAGES FROM", src_dir)
+    for filename in os.listdir(src_dir):
+        name, extension = os.path.splitext(filename)
+        if extension != '.jpg':
+            continue
+        resize(src_dir, name, extension)
 
 
 if __name__ == '__main__':
