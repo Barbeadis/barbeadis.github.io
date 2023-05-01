240123565c235d6c989e242151bafdf8a67ae460
Marco De Paoli <depaolim@gmail.com>
2023-05-01 16:56:06 +0200

    till 82

--- utils/scale_down.py
+++ utils/scale_down.py
@@ -1,31 +1,46 @@
 import os
 import sys
 
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 
 DESTS = {
     2: {"img": (400, 400)},
     3: {"img": (500, 500)},
     16: {"img": (200, 200)},
     19: {"img": (500, 500)},
+    79: {"img": (500, 500)},
+    82: {"img": (500, 500)},
 }
-
 DEST_DEFAULTS = {
     "img_big": (800, 800),
     "img": (300, 300),
 }
+TEXTS = {
+    75: "Pasqua 2022",
+    78: "Mandi Mandi!",
+}
+TEXT_COLOR = "violet"
 
 
 def resize(src_dir, name, extension):
+    index = int(name)
     src_path = os.path.join(src_dir, name + extension)
     im = Image.open(src_path)
-    dests = DESTS.get(int(name), {})
+    dests = DESTS.get(index, {})
     for dest_dir, max_size_default in DEST_DEFAULTS.items():
         max_size = dests.get(dest_dir, max_size_default)
         dst_path = os.path.join(dest_dir, name + extension)
         print(dst_path, max_size)
         im.thumbnail(max_size, Image.LANCZOS)
         # im.resize(max_size, Image.LANCZOS)
+        text = TEXTS.get(index)
+        if dest_dir == 'img' and text:
+            left, top, right, bottom = im.getbbox()
+            print(index, text, left, top, right, bottom)
+            font = ImageFont.truetype("Verdana.ttf", size=25)
+            draw = ImageDraw.Draw(im)
+            draw.text((left + 50, bottom - 50), text, fill=TEXT_COLOR, font=font)
+            del draw
         im.save(dst_path)
 
 
